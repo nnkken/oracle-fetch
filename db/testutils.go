410e1869021135719c2b05871d9e7a3b90629d3b
Chung Wu <imwmbm@gmail.com>
2023-03-20 11:17:20 +0800

    ✅ Add test cases for inserter

---
+++ db/testutils.go
@@ -0,0 +1,70 @@
+package db
+
+import (
+	"context"
+	"errors"
+	"os"
+	"testing"
+
+	"github.com/jackc/pgx/v5"
+)
+
+const DefaultTestDbURL = "postgres://postgres:postgres@localhost:5432/postgres"
+
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
+
+func SetupTestConn(t *testing.T) *pgx.Conn {
+	dbURL := getEnv("PG_TEST_DB_URL", DefaultTestDbURL)
+	conn, err := pgx.Connect(context.Background(), dbURL)
+	if err != nil {
+		panic(err)
+	}
+	rows, err := conn.Query(context.Background(), "SELECT 1 FROM information_schema.tables WHERE table_schema = 'public'")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+	if rows.Next() {
+		panic(errors.New("testing database is not empty, possibly testing on production database"))
+	}
+	err = RunMigrations(dbURL)
+	if err != nil {
+		panic(err)
+	}
+	t.Cleanup(func() {
+		// conn could be closed so we make a new one
+		conn, err := pgx.Connect(context.Background(), dbURL)
+		if err != nil {
+			panic(err)
+		}
+		defer conn.Close(context.Background())
+		var tables []string
+		rows, err := conn.Query(context.Background(), "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
+		if err != nil {
+			panic(err)
+		}
+		defer rows.Close()
+		for rows.Next() {
+			var tableName string
+			err := rows.Scan(&tableName)
+			if err != nil {
+				panic(err)
+			}
+			tables = append(tables, tableName)
+		}
+		rows.Close() // for freeing the connection; it's OK to duplicate close as stated in pgx docs
+		for _, tableName := range tables {
+			_, err := conn.Exec(context.Background(), "DROP TABLE "+tableName)
+			if err != nil {
+				panic(err)
+			}
+		}
+		conn.Close(context.Background())
+	})
+	return conn
+}
--- runner/runner_test.go
+++ runner/runner_test.go
@@ -1,6 +1,7 @@
 package runner
 
 import (
+	"context"
 	"errors"
 	"math/big"
 	"testing"
@@ -8,41 +9,71 @@ import (
 
 	"go.uber.org/zap"
 
-	"github.com/nnkken/oracle-fetch/types"
 	"github.com/stretchr/testify/require"
+
+	"github.com/nnkken/oracle-fetch/db"
+	"github.com/nnkken/oracle-fetch/types"
 )
 
-func TestInsert(t *testing.T) {
+var testEntries = []types.DBEntry{
+	{
+		Token:          "TEST",
+		Unit:           "TEST-UNIT",
+		Price:          big.NewInt(1234),
+		PriceTimestamp: time.Unix(1234567890, 0),
+		FetchTimestamp: time.Unix(1300000000, 0),
+	},
+	{
+		Token:          "TEST-2",
+		Unit:           "TEST-2-UNIT",
+		Price:          big.NewInt(5678),
+		PriceTimestamp: time.Unix(1234567891, 0),
+		FetchTimestamp: time.Unix(1300000001, 0),
+	},
+}
 
+func TestInsert(t *testing.T) {
+	conn := db.SetupTestConn(t)
+	defer conn.Close(context.Background())
+
+	insert(testEntries[0], conn, zap.S())
+	insert(testEntries[1], conn, zap.S())
+
+	rows, err := conn.Query(context.Background(), "SELECT token, unit, price, price_timestamp, fetch_timestamp FROM prices ORDER BY id")
+	require.NoError(t, err)
+	hasNextRow := rows.Next()
+	require.True(t, hasNextRow)
+	var rowEntry types.DBEntry
+	var priceStr string
+	err = rows.Scan(&rowEntry.Token, &rowEntry.Unit, &priceStr, &rowEntry.PriceTimestamp, &rowEntry.FetchTimestamp)
+	require.NoError(t, err)
+	priceFloat, _, err := new(big.Float).Parse(priceStr, 10)
+	require.NoError(t, err)
+	rowEntry.Price, _ = priceFloat.Int(nil)
+	require.Equal(t, testEntries[0], rowEntry)
+	hasNextRow = rows.Next()
+	require.True(t, hasNextRow)
+	err = rows.Scan(&rowEntry.Token, &rowEntry.Unit, &priceStr, &rowEntry.PriceTimestamp, &rowEntry.FetchTimestamp)
+	require.NoError(t, err)
+	priceFloat, _, err = new(big.Float).Parse(priceStr, 10)
+	require.NoError(t, err)
+	rowEntry.Price, _ = priceFloat.Int(nil)
+	require.Equal(t, testEntries[1], rowEntry)
+	hasNextRow = rows.Next()
+	require.False(t, hasNextRow)
 }
 
 func TestFetch(t *testing.T) {
 	mockDataSource := types.NewMockDataSource(t)
-	expected := []types.DBEntry{
-		{
-			Token:          "TEST",
-			Unit:           "TEST-UNIT",
-			Price:          big.NewInt(1234),
-			PriceTimestamp: time.Unix(1234567890, 0),
-			FetchTimestamp: time.Unix(1300000000, 0),
-		},
-		{
-			Token:          "TEST-2",
-			Unit:           "TEST-2-UNIT",
-			Price:          big.NewInt(5678),
-			PriceTimestamp: time.Unix(1234567891, 0),
-			FetchTimestamp: time.Unix(1300000001, 0),
-		},
-	}
-	mockCall := mockDataSource.EXPECT().Fetch().Return(expected, nil)
+	mockCall := mockDataSource.EXPECT().Fetch().Return(testEntries, nil)
 
 	ch := make(chan types.DBEntry, 100)
 	fetch(mockDataSource, ch, zap.S())
 	require.Len(t, ch, 2)
 	entry := <-ch
-	require.Equal(t, expected[0], entry)
+	require.Equal(t, testEntries[0], entry)
 	entry = <-ch
-	require.Equal(t, expected[1], entry)
+	require.Equal(t, testEntries[1], entry)
 	mockCall.Unset()
 
 	mockDataSource.EXPECT().Fetch().Return(nil, errors.New("err"))
